59883bbb9d86c1c60685c3340e1d1ca8df82bdb0
Maxime Wang <mwang@localhost.localdomain>
2017-11-16 11:53:22 +0100

    fct error

---
+++ my_error.c
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2017
+** my_error.c
+** File description:
+** Gestion d'erreur
+*/
+
+int my_paramerror(int ac, char **str)
+{
+	int j = 0;
+	if (ac != 8)
+		return (84);
+	while (str[7][j] != '\0') {
+		if (str[7][j] == '.' && str[7][j] == '-')
+			return (84);
+		j = j + 1;
+	}
+	return (0);
+}
+
